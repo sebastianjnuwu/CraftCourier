a4dfc981e4405475f5f143badbd92fd885e6d008
SEBASTIAN JN ฅ^•ﻌ•^ฅ <sebastianjnuwu@gmail.com>
2022-05-09 12:57:57 -0300

    update Belly.java

--- src/main/java/io/kettra/plugins/belly/Belly.java
+++ src/main/java/io/kettra/plugins/belly/Belly.java
@@ -1,6 +1,5 @@
 package io.kettra.plugins.belly;
 
-// importando as dependências!
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -14,7 +13,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-// classe principal!
+
 public class Belly extends JavaPlugin {
     
     // conexão com mysql!
@@ -22,11 +21,13 @@ public class Belly extends JavaPlugin {
     
     @Override // ação de quando o plugin é ligado!
     public void onEnable() {
-		// salvar e carregar as config.yml
+		
        saveDefaultConfig();
-	     reloadConfig();
+	   reloadConfig();
+       
        // mensagem no console quando o plugin é ligado
        getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §aPlugin Ligado com sucesso!");
+      
        try {
 	// conexão com mysql
  con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("MySQL.host") + "/" + getConfig().getString("MySQL.database"), getConfig().getString("MySQL.usuario"), getConfig().getString("MySQL.senha"));
@@ -35,14 +36,57 @@ public class Belly extends JavaPlugin {
    getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §cNão foi possível conectar ao MySQL: " + e.getMessage());
    setEnabled(true);
    return;
- }
-}
-
-    @Override
-    // quando o plugin é desligado! açao que ele ira realizar ;-;
-    public void onDisable() {
+   
+      }
       
-      // mensagem no console de quando o plugin é desligado!
-     getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §cPlugin Desligado com sucesso!");
+      new BukkitRunnable() {
+
+			@Override
+			public void run() {
+				try {
+					check();
+					
+				} catch (SQLException e) {
+					getLogger().severe("Erro ao realizar checagem: " + e.getMessage());
+				}
+			}
+		}.runTaskTimerAsynchronously(this, getConfig().getInt("tempo") * 1200L, getConfig().getInt("tempo") * 1200L);
+    }
+    
+    private void check() throws SQLException {
+
+		PreparedStatement check = con.prepareStatement("SELECT * FROM `transacao` WHERE `nick` = ?");
+        
+        for (Player p : Bukkit.getOnlinePlayers()) {
+			check.setString(1, p.getName());
+			ResultSet rs = check.executeQuery();
+			
+		if (rs.next()) {
+				String code = rs.getString("uuid");
+				int productCode = rs.getInt("id_produto");
+				String product = Integer.toString(productCode);
+			
+		if (getConfig().isList(product + ".commands")) {
+			new BukkitRunnable() {
+
+			@Override
+			public void run() {
+					for (String cmd : getConfig().getStringList(product + ".commands")) {
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd.replace("@player", p.getName()));
+					}
+				}	
+			
+				}.runTaskLater(this, 1L);
+			}
+		}
+	}
+}
+		
+    @Override
+   
+    public void onDisable() {
+        
+        getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §cPlugin Desligado com sucesso!");
+        
     }
 }
