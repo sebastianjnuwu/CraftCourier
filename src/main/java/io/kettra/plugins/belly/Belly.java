d158765102e3c082325fc31fee2874c61f24d3db
sebastianjnuwu <sebastianjnuwu@gmail.com>
2022-07-28 15:17:59 -0300

    feat: new version we update the database and put history hope it is the last commit

--- src/main/java/io/kettra/plugins/belly/Belly.java
+++ src/main/java/io/kettra/plugins/belly/Belly.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Timestamp;
 import java.sql.SQLException;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -17,45 +18,47 @@ public class Belly extends JavaPlugin {
     
     private Connection con;
     
-    @Override
-    public void onEnable() {
+  @Override
+  public void onEnable() {
 		
 		int pluginId = 15226; 
-        Metrics metrics = new Metrics(this, pluginId);
+    Metrics metrics = new Metrics(this, pluginId);
         
-         saveDefaultConfig();
-	     reloadConfig();
+    saveDefaultConfig();
+	  reloadConfig();
        
-       getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §aPlugin connected successfully!");
+  getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §aPlugin connected successfully!");
       
-       try {
+    try {
            
- con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("MySQL.host") + "/" + getConfig().getString("MySQL.database"), getConfig().getString("MySQL.usuario"), getConfig().getString("MySQL.senha"));
+  con = DriverManager.getConnection("jdbc:mysql://" + getConfig().getString("MySQL.host") + "/" + getConfig().getString("MySQL.database"), getConfig().getString("MySQL.usuario"), getConfig().getString("MySQL.senha"));
 
- } catch (SQLException e) {
-   getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §cCould not connect to MySQL: " + e.getMessage());
-   setEnabled(false);
+  } catch (SQLException e) {
+  getServer().getConsoleSender().sendMessage("§f[Ket§btr§ca§aSh§bop§f] §f- §cCould not connect to MySQL: " + e.getMessage());
+   setEnabled(true);
    return;
-   
-      }
+ }
       
-      new BukkitRunnable() {
-			@Override
-			public void run() {
-				try {
-					check(); 
-				} catch (SQLException e) {
+ new BukkitRunnable() {
+  	@Override
+		public void run() {
+		 	try {
+		  	check(); 
+    	} catch (SQLException e) {
 					getLogger().severe(" - houve um erro: " + e.getMessage());
 				}
 			}
 		}.runTaskTimerAsynchronously(this, getConfig().getInt("tempo") * 1200L, getConfig().getInt("tempo") * 1200L);
     }
     
-    private void check() throws SQLException {
+  private void check() throws SQLException {
 
-		PreparedStatement check = con.prepareStatement("SELECT * FROM transacao WHERE nick = ? and status_transacao = '2'");
+	PreparedStatement check = con.prepareStatement("SELECT * FROM transaction WHERE nick = ? and status = '2'");
 		
-        PreparedStatement delete = con.prepareStatement("DELETE FROM transacao WHERE uuid = ?");
+  PreparedStatement delete = con.prepareStatement("DELETE FROM transaction WHERE uuid = ?");
+  
+  PreparedStatement log = con.prepareStatement("INSERT INTO `historic` (`uuid`, `nick`, `id_product`, `date`, `status`)"
+				+ " VALUES (?, ?, ?, ?, 2)");
 
     for (Player p : Bukkit.getOnlinePlayers()) {
 			check.setString(1, p.getName());
@@ -63,14 +66,20 @@ public class Belly extends JavaPlugin {
 			
 		if (rs.next()) {
 				String code = rs.getString("uuid");
-				int productCode = rs.getInt("id_pacote");
+				int productCode = rs.getInt("id_product");
 				String product = Integer.toString(productCode);
 			
 		if (getConfig().isList(product + ".commands")) {
 		    
-		    delete.setString(1, code);
+		  delete.setString(1, code);
 			delete.executeUpdate();
-		
+		  
+		  log.setString(1, code);
+		  log.setString(2, p.getName());
+	  	log.setInt(3, productCode);
+			log.setTimestamp(4, new Timestamp(System.currentTimeMillis()));
+			log.executeUpdate();
+					
    p.sendMessage(getConfig().getString("mensagem").replaceAll("&", "§").replaceAll("@player", p.getName()));
    
    DiscordWebhook webhook = new DiscordWebhook(getConfig().getString("webhook.url"));
