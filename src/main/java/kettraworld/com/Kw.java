65a954459e79aeff8333f347c232988f2cf596a8
SEBASTIAN JN <sebastianjnuwu@gmail.com>
2023-06-04 05:47:55 -0300

    update src/main/java/kettraworld/com/Kw.java

--- src/main/java/kettraworld/com/Kw.java
+++ src/main/java/kettraworld/com/Kw.java
@@ -30,7 +30,7 @@ public class Kw extends JavaPlugin {
 		public void run() {
 		 	try {
 		  	new task();
-		 getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&r&b&l☽ &r&b&lK&r&b&le&r&b&lt&r&b&lt&r&f&lr&r&f&la&r&f&lW&r&7&lo&r&7&lr&r&d&ll&r&5&ld &r&5&lﾠ&a✧ Executando task " + (n.nextInt(1000) + 1) * 3));
+		 getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&r&b&l☽ &r&b&lK&r&b&le&r&b&lt&r&b&lt&r&f&lr&r&f&la&r&f&lW&r&7&lo&r&7&lr&r&d&ll&r&5&ld &r&5&lﾠ&a✧ Executing task " + (n.nextInt(1000) + 1) * 3));
     	} catch (Exception e) {
 					getLogger().severe(" • houve um erro: " + e.getMessage());
 				}
@@ -41,7 +41,7 @@ public class Kw extends JavaPlugin {
    @Override
   public void onDisable() {
    
-   getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&r&b&l☽ &r&b&lK&r&b&le&r&b&lt&r&b&lt&r&f&lr&r&f&la&r&f&lW&r&7&lo&r&7&lr&r&d&ll&r&5&ld &r&5&lﾠ&c✧ Disconectado do cluster!"));
+   getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&r&b&l☽ &r&b&lK&r&b&le&r&b&lt&r&b&lt&r&f&lr&r&f&la&r&f&lW&r&7&lo&r&7&lr&r&d&ll&r&5&ld &r&5&lﾠ&c✧ disconnected from cluster"));
   
   }
     
